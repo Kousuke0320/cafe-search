310313b1b6bc14973b6c257206b2212d3a44b9ab
田中康介 <tanakakousuke@tanakakousukenoMacBook-Pro.local>
2019-07-10 23:53:40 +0900

    first commit

---
+++ sapleiosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sapleiosApp
+//
+//  Created by 田中康介 on 2019/07/10.
+//  Copyright © 2019 田中康介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
