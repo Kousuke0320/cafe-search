c41af95693ed593e2fe723a2a74902f29c867d55
田中康介 <tanakakousuke@tanakakousukenoMacBook-Pro.local>
2019-08-04 20:20:29 +0900

    表示できた

--- sapleiosApp/ViewController.swift
+++ sapleiosApp/ViewController.swift
@@ -11,36 +11,53 @@ import RxSwift
 import RxCocoa
 
 struct Article: Codable {
-    var rest: [Rest]
+    //初期化をしないと表示されない
+    init() {
+        
+    }
+    var rest: [Rest]?
 }
 
 struct Rest: Codable {
-    var id: String
+    var name: String?
+    var address: String?
 }
 
-//extension Article {
-////    init(_ json: [String: Any]) {
-////        if let rest = json["rest"] as? [String: Any] {
-////            if let id = rest["id"] as? String {
-////                self.rest = id
-////            }
-////        }
-////    }
-//}
+//描画に使用
+extension Article {
+    init(_ json: [String: Any]) {
+        if let article = json["rest"] as? [Rest] {
+            self.rest = article
+        }
+    }
+}
+
+extension Rest {
+    init(_ json: [String: Any]) {
+        if let name = json["name"] as? String {
+            self.name = name
+        }
+        if let address = json["address"] as? String {
+            self.address = address
+        }
+    }
+}
 
 struct Qiita {
     
-    static func fetchArticle(completion: @escaping (Article?) -> Swift.Void) {
+    
+    static func fetchArticle(name: String? ,completion: @escaping (Article?) -> Swift.Void) {
         
-        let url =  "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=68d888e65ff9a737216fd6d084c28179&name=tokyo"
-        //URLが無効ならreturnを返す
-        guard var urlComponents = URLComponents(string: url) else {
+        guard let name = name else {
             return
         }
         
-        urlComponents.queryItems = [
-            URLQueryItem(name: "per_page", value: "50")
-        ]
+        let url =  "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=68d888e65ff9a737216fd6d084c28179&name=" + name
+        //URLが無効ならreturnを返す
+        guard let urlComponents = URLComponents(string: url) else {
+            return
+        }
+        
         
         let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, response, error in
             
@@ -50,7 +67,7 @@ struct Qiita {
             
             do {
                 let article = try JSONDecoder().decode(Article.self, from: jsonData)
-                print("\(article)")
+                //completion(article)
                 completion(article)
                 
             } catch {
@@ -68,20 +85,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
     
     
-    var articles: Article?
+    var articles: Article = Article()
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
+        setUpTableView: do {
+            tableView.frame = view.frame
+            tableView.dataSource = self
+            view.addSubview(tableView)
+        }
         
-        Qiita.fetchArticle(completion: { (articles) in
+        Qiita.fetchArticle(name: textField.text, completion: { (articles) in
             guard let articleValue = articles else {
                 return
             }
-            
-            self.articles?.rest = articleValue.rest
+            //なぜかnilがはいる
+            self.articles = articleValue
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -89,8 +110,8 @@ class ViewController: UIViewController {
         })
         
         button.rx.tap.subscribe{ [unowned self] _ in
+            
             self.tableView.reloadData()
-            print("tapp")
             }
             .disposed(by: disposeBag)
         
@@ -105,16 +126,17 @@ extension ViewController: UITableViewDataSource {
     //cellをセットする
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-//        let article = articles[indexPath.row]
-//        cell.textLabel?.text = article.id
-        //cell.detailTextLabel?.text = article.userId
-        let article = articles?.rest[indexPath.row]
-        cell.textLabel?.text = article?.id
+        let article = articles
+        let rest = article.rest?[indexPath.row]
+        cell.textLabel?.text = rest?.name
         return cell
     }
     
     //cellの数をセットする
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles?.rest.count ?? 0
+//        guard let rest = articles?.rest else {
+//            return 0
+//        }
+        return articles.rest?.count ?? 0
     }
 }
