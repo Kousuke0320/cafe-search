9ff28a6bb55cbdc5d60e09f2eed6ebdf12ce94e8
田中康介 <tanakakousuke@tanakakousukenoMacBook-Pro.local>
2019-08-06 00:54:01 +0900

    画像追加

--- sapleiosApp/ViewController.swift
+++ sapleiosApp/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import Lottie
 
 struct Article: Codable {
     //初期化をしないと表示されない
@@ -22,6 +21,12 @@ struct Article: Codable {
 struct Rest: Codable {
     var name: String?
     var address: String?
+    var image_url: Image?
+}
+
+struct Image: Codable {
+    var shop_image1: String?
+    var shop_image2: String?
 }
 
 //描画に使用
@@ -40,6 +45,21 @@ extension Rest {
         }
         if let address = json["address"] as? String {
             self.address = address
+        }
+        if let image_url = json["image_url"] as? Image {
+            self.image_url = image_url
+        }
+    }
+}
+
+extension Image {
+    init(_ json: [String: Any]) {
+        
+        if let shop_image1 = json["shop_image1"] as? String {
+            self.shop_image1 = shop_image1
+        }
+        if let shop_image2 = json["shop_image2"] as? String {
+            self.shop_image2 = shop_image2
         }
     }
 }
@@ -93,18 +113,6 @@ class ViewController: UIViewController {
     var articles: Article = Article()
     let disposeBag = DisposeBag()
     
-//    func showAnimation() {
-//        let animationView = AnimationView(name: "Animation")
-//        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
-//        animationView.center = self.view.center
-//        //animationView.loopAnimation = false
-//        animationView.contentMode = .scaleAspectFit
-//        animationView.animationSpeed = 1
-//
-//        view.addSubview(animationView)
-//
-//        animationView.play()
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -148,6 +156,21 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let article = articles
         let rest = article.rest?[indexPath.row]
         cell.textLabel?.text = rest?.name
+//        if let url = rest?.image_url?.shop_image1 {
+//            cell.imageView?.image = UIImage(named: url)
+//        }
+        guard let stringUrl = rest?.image_url?.shop_image1 else {
+            return cell
+        }
+        
+        let url = URL(string: stringUrl)
+        do {
+            let data = try Data(contentsOf: url!)
+            let image = UIImage(data: data)
+            cell.imageView?.image = image
+        }catch let err {
+            print("Error : \(err.localizedDescription)")
+        }
         return cell
     }
     
